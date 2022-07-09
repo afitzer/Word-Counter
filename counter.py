eaec0e18e8c92c77c9ef82cc32488433560e2e8e
afitzer <alexpfitzgerald@yahoo.com>
2022-07-09 15:06:27 -0500

    populated the code and texts

---
+++ counter.py
@@ -0,0 +1,18 @@
+name = input('Enter file: ')
+handle = open(name, 'r')
+counts = dict()
+
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+# Sort the dictionary by value
+lst = list()
+for k, v in list(counts.items()):
+    lst.append((v, k))
+
+lst.sort(reverse=True)
+
+for k, v in lst[:30]:
+    print(v, k)
